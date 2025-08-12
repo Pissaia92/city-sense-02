677832e4d2e316e0233f814019314158a07e0a13
Pissaia <cj.nord@yahoo.com>
2025-08-12 17:23:45 -0300

    fixes

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://city-sense.vercel.app/"],
+    allow_origins=["https://city-sense.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
